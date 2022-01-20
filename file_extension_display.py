eeefae3c5b688bad37c5fd8fea0494211dc00d7d
Sphagetto_ino <73871949+sphagettoino@users.noreply.github.com>
2022-01-20 23:08:41 +0530

    Upload initial files

---
+++ file_extension_display.py
@@ -0,0 +1,3 @@
+f = input("Input the filename with extension: ")
+f_extnsn = f.split(".")
+print("The extension of the entered file is "+ "." + f_extnsn[-1])
---
+++ upside_down_name.py
@@ -0,0 +1,3 @@
+str1 = input("Enter your name: ")
+str2,str3 = str1.split()
+print(str3 + " " + str2)
